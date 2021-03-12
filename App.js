4f47006503c8e777e6e035051c4c8e84f8ad7575
mohamedghaly140 <mohamedghaly140@gmail.com>
2021-03-13 00:33:35 +0200

    setup global theme

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
+import { theme } from './src/infrastructure/theme';
+import { ThemeProvider } from 'styled-components/native';
+
+// Screens
 import RestaurantScreen from './src/features/restaurants/screens/Restaurant';
 
 export default function App() {
   return (
     <>
-      <RestaurantScreen />
+      <ThemeProvider theme={theme}>
+        <RestaurantScreen />
+      </ThemeProvider>
       <StatusBar style='auto' />
     </>
   );
