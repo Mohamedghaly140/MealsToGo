c150812589375cdbafb0a5c96f2450923bdb52c7
mohamedghaly140 <mohamedghaly140@gmail.com>
2021-04-20 01:44:00 +0200

    add register and loading spinner

--- src/features/account/screens/Login.js
+++ src/features/account/screens/Login.js
@@ -1,4 +1,5 @@
 import React, { useState, useContext } from 'react';
+import { ActivityIndicator, Colors } from 'react-native-paper';
 import {
   AccountBackground,
   AccountCover,
@@ -15,7 +16,7 @@ import { AuthenticationContext } from '../../../services/authentication/context'
 const LoginScreen = ({ navigation }) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const { onLogin, error } = useContext(AuthenticationContext);
+  const { onLogin, isLoading, error } = useContext(AuthenticationContext);
 
   return (
     <AccountBackground>
@@ -37,7 +38,6 @@ const LoginScreen = ({ navigation }) => {
             textContentType='password'
             secureTextEntry
             autoCapitalize='none'
-            secure
             onChangeText={p => setPassword(p)}
           />
         </Spacer>
@@ -47,13 +47,17 @@ const LoginScreen = ({ navigation }) => {
           </ErrorContainer>
         )}
         <Spacer size='large'>
-          <AuthButton
-            icon='lock-open-outline'
-            mode='contained'
-            onPress={() => onLogin(email, password)}
-          >
-            Login
-          </AuthButton>
+          {!isLoading ? (
+            <AuthButton
+              icon='lock-open-outline'
+              mode='contained'
+              onPress={() => onLogin(email, password)}
+            >
+              Login
+            </AuthButton>
+          ) : (
+            <ActivityIndicator animating={true} color={Colors.blue300} />
+          )}
         </Spacer>
       </AccountContainer>
       <Spacer size='large'>
